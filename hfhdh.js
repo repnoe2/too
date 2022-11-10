727178517400fbde34bbf0cc644956da265827a3
repnoe2 <115735429+repnoe2@users.noreply.github.com>
2022-11-10 19:48:38 +0300

    Add files via upload

--- hfhdh.js
+++ hfhdh.js
@@ -1,4 +1,7 @@
-let v,i;
-i=5;
-v=99999999999;
-alert(i+v)
+function a() {
+let a= Number(docume.getElementById('a').value);
+let b= Number(docume.getElementById('b').value);
+let c=a+b;
+document.getElementById('result').innerHTML = c;
+   }
+document.getElementById('c').addEventListener('click', a);
